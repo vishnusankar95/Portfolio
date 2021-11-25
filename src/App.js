90a136d8874319a5a99b7b5bd389834a239b471f
vishnusankarmasai <82999551+vishnusankarmasai@users.noreply.github.com>
2021-11-25 22:47:45 +0530

    Initial test

--- src/App.js
+++ src/App.js
@@ -3,32 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <div>
-      <h1>Resume</h1>
-      <a href="https://drive.google.com/file/d/1vEgflgGh5p5JsWb0nvfq2FKTCu7SfM_l/view?usp=sharing">Resume</a>
-    </div>
-
-    <div>
-      <h1>Linkedin</h1>
-      <a href="https://www.linkedin.com/in/vishnu-sankar-855b72b4/">Linkedin</a>
-    </div>
-
-    <div>
-      <h1>HR round 1&2</h1>
-      <a href="https://docs.google.com/document/d/1v13bq2o0oRHDWPkQRzWug-ePuvoofIu3PwHbGeta9LY/edit?usp=sharing">HR Questions</a>
-    </div>
-
-    <div>
-      <h1>GitHub</h1>
-      <a href="https://github.com/vishnusankar95">GitHub</a>
-    </div>
-
-    <div>
-      <h1>Personal Documents</h1>
-    </div>
-
-    </div>
+     <div></div>
   );
 }
 
